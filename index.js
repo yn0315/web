a047d4f7e459e4d1dca1c111783c75cd60912c14
yn0315 <yunu0315@naver.com>
2023-03-03 16:32:21 +0900

    [2023.03.03]

--- index.js
+++ index.js
@@ -245,7 +245,7 @@ app.post('/submit', function (req, res) {
             // res.header('Content-Type','text/plain');
             // res.send('200');
 
-            window.location.href = 'http://yn0315.shop';
+            location.href = 'http://yn0315.shop';
             
             // fs.readFile(__dirname+ '/public/index.html', function (error, data) {
             //     if (error) {
