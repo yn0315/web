6d4ca1228285cb1a1d6e00ff1c7a57fdf33da3cd
yn0315 <yunu0315@naver.com>
2023-03-03 15:15:36 +0900

    [3]

--- index.js
+++ index.js
@@ -249,7 +249,7 @@ app.post('/submit', function (req, res) {
                     res.writeHead(200, { 'Content-Type': 'text/html' });
                 }
             });
-        
+            res.end();
             
 
         }
