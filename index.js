2329fe8e251e9bee647b985b4d56e5c95f053521
yn0315 <yunu0315@naver.com>
2023-03-05 01:35:00 +0900

    [2023.03.05]

--- index.js
+++ index.js
@@ -243,7 +243,7 @@ app.post('/submit', function (req, res) {
             // res.header("Access-Control-Allow-Origin","http://yn0315.shop")
 
             // res.header('Content-Type','text/plain');
-            res.send('200');
+            // res.send('200');
 
             // location.href = 'http://yn0315.shop';
             // res.redirect('/');
@@ -255,15 +255,15 @@ app.post('/submit', function (req, res) {
 
             // });
 
-            // var status = {
+            var status = {
 
-            //     "status": 200,
+                "status": 200,
             
-            //     "message": 'login success'
+                "message": 'success'
             
-            //   }
+              }
             
-            //   res.end(JSON.stringify(status));
+              res.end(JSON.stringify(status));
             
             
 
